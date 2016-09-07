6ec70d053304b10e2f126935d6f137986fba6a4d
Cole Cassidy <colewcassidy@gmail.com>
2016-09-06 22:41:36 -0400

    Began API refresh
    Created multiple new php segments that serve different purposes.

--- src/api/api.php
+++ src/api/api.php
@@ -1,86 +1,8 @@
 <?php
-abstract class API
-{
-    //essential vars for API processing
-    protected $method = '';
-    protected $endpoint = '';
-    protected $verb = '';
-    protected $args = Array();
-    protected $file = Null;
-    public function __construct($request) {
-        header("Access-Control-Allow-Orgin: *");
-        header("Access-Control-Allow-Methods: *");
-        header("Content-Type: application/json");
-
-        $this->args = explode('/', rtrim($request, '/'));
-        $this->endpoint = array_shift($this->args);
-        if (array_key_exists(0, $this->args) && !is_numeric($this->args[0])) {
-            $this->verb = array_shift($this->args);
-        }
-
-        $this->method = $_SERVER['REQUEST_METHOD'];
-        if ($this->method == 'POST' && array_key_exists('HTTP_X_HTTP_METHOD', $_SERVER)) {
-            if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'DELETE') {
-                $this->method = 'DELETE';
-            } else if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'PUT') {
-                $this->method = 'PUT';
-            } else {
-                throw new Exception("Unexpected Header");
-            }
-        }
-
-        switch($this->method) {
-        case 'DELETE':
-        case 'POST':
-            $this->request = $this->_cleanInputs($_POST);
-            break;
-        case 'GET':
-            $this->request = $this->_cleanInputs($_GET);
-            break;
-        case 'PUT':
-            $this->request = $this->_cleanInputs($_GET);
-            $this->file = file_get_contents("php://input");
-            break;
-        default:
-            $this->_response('Invalid Method', 405);
-            break;
-        }
-    }
-    public function processAPI() {
-        if (method_exists($this, $this->endpoint)) {
-            return $this->_response($this->{$this->endpoint}($this->args));
-        }
-        return $this->_response("No Endpoint: $this->endpoint", 404);
-    }
-
-    private function _response($data, $status = 200) {
-        header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
-        return json_encode($data);
-    }
-
-    private function _cleanInputs($data) {
-        $clean_input = Array();
-        if (is_array($data)) {
-            foreach ($data as $k => $v) {
-                $clean_input[$k] = $this->_cleanInputs($v);
-            }
-        } else {
-            $clean_input = trim(strip_tags($data));
-        }
-        return $clean_input;
-    }
-
-    private function _requestStatus($code) {
-        $status = array(
-            200 => 'OK',
-            404 => 'Not Found',
-            405 => 'Method Not Allowed',
-            500 => 'Internal Server Error',
-        );
-        return ($status[$code])?$status[$code]:$status[500];
-    }
-}
+require_once 'apiInterpreter.php';
 class MyAPI extends API {
+//from here down are endpoint definitions
+//$endpoint/$verb/$args/$args
   protected function example() {
     if ($this->method == 'GET') {
       return "Hello World";
@@ -88,5 +10,23 @@ class MyAPI extends API {
       return "Nope!!";
     }
   }
+  protected function apiwall() {
+    if ($this->method == 'GET') {
+      return "<html>API server Online. <br/> Check docs for help.</html>";
+    } else {
+      return "Nope!!";
+    }
+  }
+}
+
+if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+}
+
+try {
+    $API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    echo $API->processAPI();
+} catch (Exception $e) {
+    echo json_encode(Array('error' => $e->getMessage()));
 }
 ?>
---
+++ src/api/apiInterpreter.php
@@ -0,0 +1,81 @@
+<?php
+abstract class API
+{
+    //essential vars for API processing
+    protected $method = '';
+    protected $endpoint = '';
+    protected $verb = '';
+    protected $args = Array();
+    protected $file = Null;
+    public function __construct($request) {
+
+        // Seperate the request into different vars
+        $this->args = explode('/', rtrim($request, '/'));
+        $this->endpoint = array_shift($this->args);
+        if (array_key_exists(0, $this->args) && !is_numeric($this->args[0])) {
+            $this->verb = array_shift($this->args);
+        }
+
+        // Set request method var.
+        $this->method = $_SERVER['REQUEST_METHOD'];
+
+        //if ($this->method == 'POST' && array_key_exists('HTTP_X_HTTP_METHOD', $_SERVER)) {
+        //    if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'DELETE') {
+        //        $this->method = 'DELETE';
+        //    } else if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'PUT') {
+        //        $this->method = 'PUT';
+        //    } else {
+        //        throw new Exception("Unexpected Header");
+        //    }
+        //}
+
+        switch($this->method) {
+        case 'POST':
+            $this->request = $this->cleanInputs($_POST);
+            break;
+        case 'GET':
+            $this->request = $this->cleanInputs($_GET);
+            break;
+        default:
+            $this->response('Invalid Method', 405);
+            break;
+        }
+    }
+
+    //finds endpoint and asks it to respond.
+    public function processAPI() {
+        if (method_exists($this, $this->endpoint)) {
+            return $this->response($this->{$this->endpoint}($this->args));
+        }
+        return $this->response("No Endpoint: $this->endpoint", 404);
+    }
+    //returns endpoint data
+    private function response($data, $status = 200) {
+        header("HTTP/1.1 " . $status . " " . $this->requestStatus($status));
+        return $data;
+    }
+    //removes extra header info.
+    private function cleanInputs($data) {
+        $clean_input = Array();
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $clean_input[$k] = $this->cleanInputs($v);
+            }
+        } else {
+            $clean_input = trim(strip_tags($data));
+        }
+        return $clean_input;
+    }
+    //sets status
+    private function requestStatus($code) {
+        $status = array(
+            200 => 'OK',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            500 => 'Internal Server Error',
+        );
+        return ($status[$code])?$status[$code]:$status[500];
+    }
+}
+
+?>
