74e0e56b14ee8c6da3d5a4b2521ed943fac5193d
Cole Cassidy <colewcassidy@gmail.com>
2016-09-06 23:28:53 -0400

    Created database class
    This class should unify all php database requests. Creating a smoother bridge between the client, the API, and the database.

--- src/api/database.php
+++ src/api/database.php
@@ -1,4 +1,48 @@
 <?php
-  
+class dbHost {
 
- ?>
+//essential mysql settings
+
+
+  protected static $database;
+
+  public function connect() {
+    $servername = "localhost";
+    $username = "localStratus";
+    $password = "";
+    $dbname = "Stratus";
+    $this->database = new mysqli($servername, $username, $password, $dbname);
+    if ($this->database->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    return $this->database;
+  }
+
+  public function query($query) {
+        // Connect to the database
+        $database = $this -> connect();
+
+        // Query the database
+        $result = $database-> query($query);
+
+        return $result;
+  }
+
+  public function getInv() {
+    $result = $this->query("SELECT * FROM `inventory` WHERE `inStock` = 1");
+    if ($result->num_rows > 0) {
+      // output data of each row
+
+      while($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+        //echo "id: " . $row["itemCode"]. " - Name: " . $row["name"]. " - Description: " . $row["description"]. "<br>";
+      }
+      return $rows;
+    } else {
+        echo "0 results";
+    }
+  }
+
+}
+
+?>
