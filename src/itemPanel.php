15a372623f223b4627c27142c21a221fbde4a836
Cole Cassidy <colewcassidy@gmail.com>
2016-09-07 13:11:59 -0400

    Converted html to php template.
    Allows for rapid generation of tile content.

--- src/itemPanel.php
+++ src/itemPanel.php
@@ -1,9 +1,13 @@
+<?php echo '
 <div class="col-sm-3">
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h3 class="panel-title" id="name"></h3>
+      <h3 class="panel-title" id="name">'.$x['name'].'</h3>
     </div>
     <div class="panel-body" id="description">
+    '.$x['description'].'
     </div>
   </div>
 </div>
+';
+?>
