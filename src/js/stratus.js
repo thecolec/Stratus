3721b79eaf39a7a249738cf1371c48d841011026
Cole Cassidy <thecolec@gmail.com>
2016-10-11 16:03:54 -0400

    Begin implementing color scheming

--- src/js/stratus.js
+++ src/js/stratus.js
@@ -142,6 +142,7 @@ function viewItemCard(x){
   document.getElementById("itemCardDescription").innerHTML = invJson[x].description;
   document.getElementById("itemCardPrice").innerHTML = "$"+invJson[x].price;
   document.getElementById("itemCardCode").innerHTML = "PID: "+invJson[x].itemCode;
+  document.getElementById("itemCard").style.color = palette['Muted'].getHex();
   console.log(invJson[x].itemCode);
 }
 
